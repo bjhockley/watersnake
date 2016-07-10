9269a2c1a74f29d60c30d7d0472cff12d88f2707
Ben Hockley <ben@pexip.com>
2016-07-10 20:28:37 +0100

    Unit test simulating very simple (non-SWIM) broadcast of all messages to all nodes

--- watersnake/membership.py
+++ watersnake/membership.py
@@ -14,20 +14,49 @@ def confirm(remote_member_id):
     return { "confirm" : [remote_member_id] }
 
 
-
-
-
 class MessageTransport(object):
     """ """
     def __init__(self):
         """
         """
         self.message_router = None
+        self.sent_messages = 0
+        self.received_messages = 0
 
     def register_message_router(self, message_router):
         """"""
         self.message_router = message_router
 
+    def send_message_to(self, address, message):
+        """Send message to the member identified by address"""
+        self.sent_messages += 1
+        self.send_message_impl(address, message)
+
+    def send_message_impl(self, address, message):
+        """Takes care of the nuts and bolts of message transmission"""
+        # FIXME: need to hook this up to a socket
+        pass
+
+    def on_incoming_message(self, address, message):
+        """We've received a message off the wire and need to route it to any
+        local objects that may be interested in this message. """
+        self.received_messages += 1
+        self.message_router.on_incoming_message(address, message)
+        # FIXME: need to hook this up to a socket
+
+
+class LoopbackMessageTransport(MessageTransport):
+    """ This is a specialization of MessageTransport that can only transport messages
+    to other local objects (i.e. can't use a real network).  Useful for testing. """
+    def __init__(self):
+        """
+        """
+        MessageTransport.__init__(self)
+
+    def send_message_impl(self, address, message):
+        """As all objects reachable over LoopbackMessageTransport are local, sending
+        a message can just be treated the same way as receiving a message"""
+        self.on_incoming_message(address, message)
 
 
 class MessageRouter(object):
@@ -36,18 +65,25 @@ class MessageRouter(object):
     def __init__(self, message_transport):
         """
         """
-        self.message_transport = message_transport
-        self.message_transport.register_message_router(self)
+        self.transport = message_transport
+        self.transport.register_message_router(self)
+        self.members = {}
 
-    def on_incoming_message(self):
+    def register_for_messages_for_member(self, member_id, member):
+        """ The object 'member' wishes to receive all messages routed to recipient identified by member_id
+        """
+        self.members[member_id] = member
+
+    def on_incoming_message(self, address, message):
         """
         """
-        pass
+        self.members[address].on_incoming_message(message)
 
     def send_message_to(self, remote_member, message):
         """
         """
-        pass
+        recipient_member_id = remote_member.remote_member_id
+        self.transport.send_message_to(recipient_member_id, message)
 
 
 class Membership(object):
@@ -62,20 +98,26 @@ class Membership(object):
      -  disseminating informantion about joined/left/failed members
     """
 
-    def __init__(self, local_member_id, expected_remote_members, messagerouter):
+    def __init__(self, member_id, expected_remote_members, messagerouter):
         """
         """
-        self.local_member_id = local_member_id
+        self.member_id = member_id
         self.messagerouter = messagerouter
         self.incarnation_number = 0
         self.expected_remote_members = expected_remote_members
         self.alive_remote_members = []
-        #self.broadcast_message(alive(self.local_member_id))
+        self.last_received_message = None
+        self.messagerouter.register_for_messages_for_member(self.member_id, self)
+        #self.broadcast_message(alive(self.member_id))
 
     def broadcast_message(self, message):
         """ """
         for member in self.expected_remote_members:
             self.messagerouter.send_message_to(member, message)
+
+    def on_incoming_message(self, message):
+        self.last_received_message = message
+
 
 class RemoteMember(object):
     """  Represents a remote member of the distributed process group """
--- watersnake/test/test_membership.py
+++ watersnake/test/test_membership.py
@@ -20,13 +20,26 @@ class TestWaterSnake(twisted.trial.unittest.TestCase):
 
 
     def _create_harness(self):
-        self.transport = membership.MessageTransport()
+        self.transport = membership.LoopbackMessageTransport()
         self.router = membership.MessageRouter(self.transport)
         self.members = []
         global_members = ["a", "b", "c"]
         for member_id in global_members:
             remote_members = [ membership.RemoteMember(x) for x in global_members if x != member_id ]
             self.members.append(membership.Membership(member_id, remote_members, self.router))
+
+
+    def test_simple_non_swim_propagation(self):
+        """Test simple non-swim message propagation (all nodes directly broadcast all
+        messages to all other nodes) """
+        self._create_harness()
+        sending_member = self.members[0]
+        receiving_members = self.members[1:]
+        self.assertTrue(all([recipient.last_received_message == None for recipient in receiving_members]))
+        sending_member.broadcast_message("Hello")
+        self.assertTrue(all([recipient.last_received_message == "Hello" for recipient in receiving_members]))
+        self.assertEqual(self.transport.sent_messages, 2)
+        self.assertEqual(self.transport.received_messages, 2)
 
 
     def test_instantiation(self):
