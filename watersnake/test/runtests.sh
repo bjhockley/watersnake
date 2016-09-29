871c8f491374028ce1faa695f998d9342d63173c
Ben Hockley <ben@pexip.com>
2016-09-29 16:00:14 +0100

    Output pylint rating /10  summary line after coverage report when running unit tests.

--- watersnake/test/runtests.sh
+++ watersnake/test/runtests.sh
@@ -4,6 +4,6 @@
 cd "$(dirname "$0")"
 export COVFILES=`realpath ../m*.py`
 echo $COVFILES
-PYTHONPATH=../ coverage run `which trial`  ./test_membership.py  && coverage report -m --include $COVFILES
+PYTHONPATH=../ coverage run `which trial`  ./test_membership.py  && coverage report -m --include $COVFILES && PYTHONPATH=.. pylint -i y ./test_membership.py ../membership.py | grep "Your code has been rated" 
 
 
